b8feb7bc972bf062b05047f893c895be088fd02e
blackfenix2 <devin_98@msn.com>
2020-08-02 23:05:25 -0400

    refactored project to yarn workspaces

--- src/layout/Header.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import Nav from 'src/components/Nav.svelte';
-</script>
-
-<header>
-
-  <Nav />
-</header>
--- src/layout/index.ts
+++
@@ -1,5 +0,0 @@
-import Header from './Header.svelte';
-import Body from './Body.svelte';
-import Footer from './Footer.svelte';
-
-export { Header, Body, Footer };
