aee8ef8161c93ecc6dc347b88c317f4fe8dd7041
blackfenix2 <devin_98@msn.com>
2020-08-26 00:52:31 -0400

    finished alpha compiler for .svelte files

--- packages/svelte-components/script-test.ts
+++ packages/svelte-components/script-test.ts
@@ -1,24 +1,47 @@
 import { preprocess } from 'svelte/compiler';
 import autoprocessor from 'svelte-preprocess';
 import * as fs from 'fs';
+import path from 'path';
+import glob from 'glob';
 
 const doStuff = async () => {
-  const source = fs.readFileSync(
-    `${__dirname}/src/Button/Button.svelte`,
-    'utf-8'
-  );
+  const srcPath = path.join(__dirname, 'src');
+  const distPath = path.join(__dirname, 'dist');
 
-  await preprocess(source, autoprocessor(), { filename: 'Button.svelte' })
-    .then((item) => {
-      console.log('Svelte Code: ', item.code);
-      fs.mkdirSync(`${__dirname}/dist/Button/`, {
-        recursive: true,
+  glob(
+    path.join(srcPath, '**/*.{svelte,ts,js}!(stories.ts)'),
+    {},
+    (error, files) => {
+      //handling error
+      if (error) {
+        return console.log('Unable to scan directory: ' + error);
+      }
+      //listing all files using forEach
+      files.forEach(async (file) => {
+        // juryrig to exclude storybook files
+        if (file.includes('.stories.ts')) return;
+        // Do whatever you want to do with the file
+        console.log(file);
+
+        const source = fs.readFileSync(file, 'utf-8');
+        if (file.includes('.svelte')) {
+          const distFile = file.replace('/src/', '/dist/');
+          await preprocess(source, autoprocessor(), {
+            filename: path.basename(distFile),
+          })
+            .then((item) => {
+              fs.mkdirSync(path.dirname(distFile), {
+                recursive: true,
+              });
+              fs.writeFileSync(distFile, item.code);
+            })
+            .catch((error) => {
+              console.error(error.message);
+            });
+        }
       });
-      fs.writeFileSync(`${__dirname}/dist/Button/Button.svelte`, item.code);
-    })
-    .catch((error) => {
-      console.error(error.message);
-    });
+    }
+  );
 };
 
 doStuff();
